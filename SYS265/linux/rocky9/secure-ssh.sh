eb95f094620c57d0bbaa7d8c2aeb1cd9ec8bb3ba
web01-neloy <jake.yolen@mymail.champlain.edu>
2026-03-13 14:53:27 -0400

    secure ssh

---
+++ SYS265/linux/rocky9/secure-ssh.sh
@@ -0,0 +1,7 @@
+#secure-ssh.sh
+#author neloyy
+#creates a new ssh user using $1 parameter
+#adds a  public key from the local repo or curled from the remote repo 
+#removes room ability to ssh in
+
+echo "all my code will go here"
