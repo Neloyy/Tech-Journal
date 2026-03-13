1904ea7b60308950878fc749ec9d10f1cec7b694
web01-neloy <jake.yolen@mymail.champlain.edu>
2026-03-13 15:58:48 -0400

    added code to ssh

--- SYS265/linux/rocky9/secure-ssh.sh
+++ SYS265/linux/rocky9/secure-ssh.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
 #secure-ssh.sh
 #author neloyy
 #creates a new ssh user using $1 parameter
 #adds a  public key from the local repo or curled from the remote repo 
 #removes room ability to ssh in
 
-echo "all my code will go here"
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+
+sudo mkdir /home/$1/.ssh
+sudo chmod 700 /home/$1/.ssh
+
+sudo cp /home/jake/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 600 /home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
+sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+ 
