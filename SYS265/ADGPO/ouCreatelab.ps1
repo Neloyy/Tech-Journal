9e19154a281688487f9558691bda27b8890735f9
jake yolen-adm <jake.yolen-adm@jake.local>
2026-03-16 19:57:07 -0400

    ad creation ps1

---
+++ SYS265/ADGPO/ouCreatelab.ps1
@@ -0,0 +1,9 @@
+﻿$domain = "DC=jake,DC=Local"
+$ouPath = "OU=Software Deploy, $domain"
+
+New-ADOrganizationalUnit -Name "Software Deploy" -Path $domain
+
+Get-ADComputer -Identity "wks01-jake" | Move-ADObject -TargetPath "OU=Software Deploy, DC=jake,DC=local"
+
+Set-ADOrganizationalUnit -Identity "OU=Test OU,DC=jake,DC=local" -ProtectedFromAccidentalDeletion $false
+Remove-ADOrganizationalUnit -Identity "OU=Test OU,DC=jake,DC=local" -Confirm:$false
