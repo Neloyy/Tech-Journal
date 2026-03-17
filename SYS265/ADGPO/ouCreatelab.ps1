b2a870d3f17f9f4f78db8b878ee5dd47b0ba383d
jake yolen-adm <jake.yolen-adm@jake.local>
2026-03-16 20:08:17 -0400

    fixed to add user

--- SYS265/ADGPO/ouCreatelab.ps1
+++ SYS265/ADGPO/ouCreatelab.ps1
@@ -4,6 +4,7 @@ $ouPath = "OU=Software Deploy, $domain"
 New-ADOrganizationalUnit -Name "Software Deploy" -Path $domain
 
 Get-ADComputer -Identity "wks01-jake" | Move-ADObject -TargetPath "OU=Software Deploy, DC=jake,DC=local"
+Get-ADUser -Identity "jake" | Move-ADObject -TargetPath $ouPath
 
 Set-ADOrganizationalUnit -Identity "OU=Test OU,DC=jake,DC=local" -ProtectedFromAccidentalDeletion $false
 Remove-ADOrganizationalUnit -Identity "OU=Test OU,DC=jake,DC=local" -Confirm:$false
